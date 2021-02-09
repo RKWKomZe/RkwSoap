b31f5a1e08569b3b7fc896547f983ac3d27b5689
Maximilian Faessler <maximilian@faesslerweb.de>
2021-02-09 10:29:09 +0000

    [EDIT] ext_tables update

--- ext_tables.php
+++ ext_tables.php
@@ -4,23 +4,9 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function($extKey)
     {
-        //=================================================================
-        // Register Plugin
-        //=================================================================
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'RKW.' . $extKey,
-            'Rkwsoap',
-            'RKW Soap'
-        );
 
-        //=================================================================
-        // Add TypoScript
-        //=================================================================
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-            $extKey,
-            'Configuration/TypoScript',
-            'RKW SOAP'
-        );
+
+
 
     },
     $_EXTKEY
